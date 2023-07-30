d38f3064453079caf86225fa5052e246f143dd7d
Rezwoana Ferdousy Rhea <128154797+Ferdousyrhea@users.noreply.github.com>
2023-07-30 14:59:58 +0600

    Add files via upload

---
+++ Lab01/Lab problem 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello in my university\n";
+    cout << "\n";
+    cout << "\nI'm a student in IT faculty";
+    cout << "\n";
+    cout << "\nWelcome.";
+}
+
